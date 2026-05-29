b07d69da49cf07a50a115a0e1522c7d1ee9728e1
Yui <yui-rzp@gmail.com>
2026-05-28 23:15:11 -0700

    ONE-153: bound Netdata journal logs

--- flake.nix
+++ flake.nix
@@ -433,6 +433,12 @@
               netdataCfg = hostConfig.services.netdata;
               netdataUnit = hostConfig.systemd.services.netdata;
               hermesSupplementaryGroups = pkgs.lib.toList hostConfig.systemd.services.hermes-agent.serviceConfig.SupplementaryGroups;
+              hermesUserGroups = pkgs.lib.toList hostConfig.users.users.hermes.extraGroups;
+              netdataObservePackage = builtins.head (
+                builtins.filter (
+                  pkg: pkgs.lib.getName pkg == "netdata-observe"
+                ) hostConfig.environment.systemPackages
+              );
               systemPackages = builtins.map (pkg: pkgs.lib.getName pkg) hostConfig.environment.systemPackages;
               netdataLoadCredentials = pkgs.lib.toList netdataUnit.serviceConfig.LoadCredential;
               netdataExecStartPost = pkgs.lib.toList netdataUnit.serviceConfig.ExecStartPost;
@@ -464,6 +470,14 @@
               EOF
               test '${builtins.concatStringsSep " " hermesNetdataMcp.args}' = 'ws://127.0.0.1:19999/mcp'
               test '${toString (builtins.elem "systemd-journal" hermesSupplementaryGroups)}' = '1'
+              test '${toString (builtins.elem "systemd-journal" hermesUserGroups)}' = '1'
+              '${netdataObservePackage}/bin/netdata-observe' --help | grep -q -- 'logs \[unit\] \[lines\]'
+              grep -q -- 'arbitrary journalctl arguments are not allowed' '${netdataObservePackage}/bin/netdata-observe'
+              grep -q -- 'netdata.service|hermes-agent.service|agentmemory.service|hindsight-embed.service|omp-auth-gateway.service' '${netdataObservePackage}/bin/netdata-observe'
+              grep -q -- '--output=short-iso' '${netdataObservePackage}/bin/netdata-observe'
+              ! '${netdataObservePackage}/bin/netdata-observe' logs sshd.service 10 2>/dev/null
+              ! '${netdataObservePackage}/bin/netdata-observe' logs netdata.service 501 2>/dev/null
+              ! '${netdataObservePackage}/bin/netdata-observe' logs netdata.service 10 --since=-1h 2>/dev/null
               test -d '${hostConfig.environment.etc."netdata/conf.d".source}/scripts.d'
               test -f '${hostConfig.environment.etc."netdata/conf.d".source}/scripts.d/nagios.conf'
               test '${if hostConfig.services.postgresql.enable then "true" else "false"}' = 'false'
